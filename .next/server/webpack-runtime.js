a35be0d22c650302fdfbd5d1afc05d7fc2490754
AhmedSalafi <ryankhann47@gmail.com>
2025-10-04 11:13:37 -0400

    gemini integration working properly, I can call the model now

--- .next/server/webpack-runtime.js
+++ .next/server/webpack-runtime.js
@@ -125,7 +125,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("e3c716586be85446")
+/******/ 		__webpack_require__.h = () => ("2852050f632b69cb")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
